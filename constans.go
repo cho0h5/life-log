fa502cb955a2770a08c32a186adbbb961b6ae0f9
Youngho Cho <dominoyh5@gmail.com>
2021-05-01 10:30:40 +0100

    add Docker file

--- constans.go
+++ constans.go
@@ -2,5 +2,5 @@ package main
 
 const(
 	TIME_FORMAT string = "2006-01-02 15:04:05"
-	DB_DSN = "root:secret@tcp(127.0.0.1:3306)/lifelog?charset=utf8mb4&parseTime=True&loc=Local"
+	DB_DSN = "root:secret@tcp(mysql:3306)/lifelog?charset=utf8mb4&parseTime=True&loc=Local"
 )
